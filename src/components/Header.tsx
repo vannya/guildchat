f6f458d8477d3b138afaede30bbe8a7e095bee42
Van Tabbert <van.tabbert@gmail.com>
2021-08-02 12:27:35 -0400

    Remove router connection

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router';
+import { RouteComponentProps } from 'react-router';
 export interface IHeaderProps extends RouteComponentProps {
 	isAuth: boolean;
 	onLogout: () => void;
 }
 
-export const Header = withRouter(
-	({ isAuth, onLogout, location }: IHeaderProps): JSX.Element => {
-		return (
-			<div className='header'>
-				<h1>Guild Chat</h1>
-				{isAuth ? (
-					<button onClick={onLogout}>Log out</button>
-				) : (
-					<div className='loggedout' />
-				)}
-			</div>
-		);
-	}
-);
+export const Header = ({ isAuth, onLogout }: IHeaderProps): JSX.Element => {
+	return (
+		<div className='header'>
+			<h1>Guild Chat</h1>
+			{isAuth ? (
+				<button onClick={onLogout}>Log out</button>
+			) : (
+				<div className='loggedout' />
+			)}
+		</div>
+	);
+};
